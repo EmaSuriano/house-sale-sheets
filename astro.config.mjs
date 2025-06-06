db8c22af234bd053c6c28e261fcc6f6ccf00c26d
Emanuel Suriano <emanuel.suriano@revolut.com>
2025-06-06 12:46:43 +0200

    add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://emasuriano.github.io",
+  base: "house-sale-sheets",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
